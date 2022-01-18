13a00066b93525915578e5a34430cc71cb74ec65
Surabordin <bluelimec@gmail.com>
2022-01-18 17:37:30 +0700

    lab 17-1 complete

--- lab17_1.cpp
+++ lab17_1.cpp
@@ -32,3 +32,41 @@ int main(){
 	findColSum(dPtr,sum2,N,M); 
 	showData(sum2,1,M);
 }
+void showData(double *a,int N,int M){
+    cout << fixed << setprecision(2);
+    for(int i = 0; i < N*M; i++){
+        cout << *(a+i);
+        if((i+1)%M == 0 ) cout << endl;
+        else cout << " ";
+    }
+}
+
+void randData(double *a,int N,int M){
+    for(int i = 0; i < N*M; i++){
+        *(a+i) = (rand()%101)/100.00;
+    }
+}
+
+void findRowSum(const double *a,double *b,int N,int M){
+    for(int i = 0; i< N ;i++){
+        double sum = 0;
+        for (int j=0 ; j<M ;j++){
+            sum += *((a+j)+(M*i));
+        }
+        b[i] =sum ;
+    }
+}
+
+void findColSum(const double *a,double *b,int N,int M){
+    int x = 0, y = 0 ; 
+    b[y] = 0 ;
+    for(int i = 0; i< M ;i++){
+        x = i;
+        for (int j=0 ; j<N ;j++){
+            b[y] += a[x];
+            x += M;
+        }
+        y++ ;
+        b[y] = 0 ;
+    }
+}
